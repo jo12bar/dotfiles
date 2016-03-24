2bb7687ac58785864495eda491cd49baee9b0845
jo12bar <jo12bar@users.noreply.github.com>
2016-03-24 15:11:05 -0700

    Add file `zsh/oh-my-zsh.zsh`
    Loads oh-my-zsh and antigen

---
+++ zsh/oh-my-zsh.zsh
@@ -0,0 +1,23 @@
+# Setup oh-my-zsh and antigen
+
+# Path to your oh-my-zsh installation
+export ZSH=$HOME/.oh-my-zsh
+
+HYPHEN_INSENSITIVE="true"
+ENABLE_CORRECTION="true"
+COMPLETION_WAITING_DOTS="true"
+
+source $ZSH/oh-my-zsh.sh
+
+# setup antigen
+source ~/.antigen/anitgen.zsh
+
+antigen use oh-my-zsh
+
+antigen bundle git
+antigen bundle npm
+antigen bundle sudo
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle unixorn/autoupdate-antigen.zshplugin
+
+antigen apply
