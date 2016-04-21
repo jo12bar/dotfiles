3e1e67c6796a0d16e001cde8c9de78dd9639fe4c
jo12bar <jo12bar@users.noreply.github.com>
2016-04-20 17:49:29 -0700

    Add tmux, and make it play nicely with vim & solarized
    On a different note, I;m now using Konsole, for no particular reason whatsoever.
    As in, it litterally has no benifits for me that gnome-terminal doesn't already
    have. Oh well. I'm stubborn.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,4 @@
 alias reload!='. ~/.zshrc'
+
+# Make sure that tmux uses 256 color mode
+alias tmux="tmux -2"
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,1 +1,5 @@
 export PATH="$DOTFILES_ZSH/bin:$PATH"
+
+# Set the term so that tmux & vim play together nicely
+# (I know I shouldn't be doing this, but these are my dotfiles fool!)
+export TERM="screen-256color"
