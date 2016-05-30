673a73f47f639595f482aa06671daaa60c62178e
jo12bar <jo12bar@users.noreply.github.com>
2016-05-30 07:28:55 -0700

    Revamp node/install.bash

--- node/install.bash
+++ node/install.bash
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 source "$DOTFILES_ZSH/script/lib/info_printer.bash"
 
-npm install --global npm
-npm install --global npm-check
-npm install --global diff-so-fancy
-npm install --global hotel
+packages=(npm npm-check diff-so-fancy hotel)
+
+text_info node/install.bash "Installing the following global npm packages:"
+for package in ${packages[*]}
+do
+  text_info node/install.bash "  - $package"
+done
+
+for package in ${packages[*]}
+do
+  npm install --global $package
+done
