dd359cb3549600177b90ac45bc12bee65a95623e
jo12bar <jo12bar@users.noreply.github.com>
2016-06-09 18:27:34 -0700

    Stop loading oh-my-zsh twice
    Apparently I was loading oh-my-zsh twice by sourcing `~/.oh-my-zsh/oh-my-zsh.sh` and then immediately loading it in Antigen. This change should help make things a little bit faster :)

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -1,23 +1,21 @@
 # Setup oh-my-zsh and antigen
+# setup antigen
+source ~/.antigen/antigen.zsh
 
-# Path to your oh-my-zsh installation
-export ZSH=$HOME/.oh-my-zsh
-
+# Set oh-my-zsh variables
 HYPHEN_INSENSITIVE="true"
 ENABLE_CORRECTION="true"
 COMPLETION_WAITING_DOTS="true"
 
-source $ZSH/oh-my-zsh.sh
-
-# setup antigen
-source ~/.antigen/antigen.zsh
-
+# Load oh-my-zsh
 antigen use oh-my-zsh
 
+# Load oh-my-zsh plugins
 antigen bundle git
 antigen bundle npm
 antigen bundle sudo
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle unixorn/autoupdate-antigen.zshplugin
 
+# Done!
 antigen apply
