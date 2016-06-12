724284abd6600e2ee97932222cee9f38d01a6dbb
jo12bar <jo12bar@users.noreply.github.com>
2016-06-11 18:28:47 -0700

    Add `tldr` npm package

--- node/install.bash
+++ node/install.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source "$DOTFILES_ZSH/script/lib/info_printer.bash"
 
-packages=(npm npm-check diff-so-fancy hotel)
+packages=(npm npm-check diff-so-fancy hotel tldr)
 
 text_info node/install.bash "Installing the following global npm packages:"
 for package in ${packages[*]}
